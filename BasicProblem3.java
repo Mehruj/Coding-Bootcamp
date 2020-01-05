ab51009b518399337ba41743c1fcc9610dec8d7e
Mehruj <49351612+Mehruj@users.noreply.github.com>
2020-01-04 23:19:21 -0500

    Add files via upload

---
+++ BasicProblem3.java
@@ -0,0 +1,13 @@
+package BasicProblems;
+
+public class BasicProblem3 {
+	 public boolean only14(int [] nums){
+		  for(int i=0; i<nums.length;i++) {
+			  if(nums[i]!=1 || nums[i]!=4) {
+				  return false;
+			  }
+		  }
+		  return true;
+	 }
+
+}
